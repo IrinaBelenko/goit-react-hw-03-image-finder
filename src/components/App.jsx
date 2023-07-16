812a2a2769f32ed8fbb0314dac2fb2a8174f3d05
IrinaBelenko <118209690+IrinaBelenko@users.noreply.github.com>
2023-07-16 22:28:15 +0300

    test api

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,16 @@
+import { getImages } from 'service/image-service';
+
+const getImagesTest = async (query, page) => {
+  try {
+    const test = await getImages(query, page);
+    console.log(test);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+getImagesTest('cat', 1);
+
 export const App = () => {
   return (
     <div
@@ -7,7 +20,7 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
       React homework template
